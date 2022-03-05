f5cf79bdcb647d1c4ba1650c31469934eb470940
Allan Galera <allangalera@gmail.com>
2022-03-05 16:03:24 -0300

    fix: ascension start too big on chrome

--- src/lib/components/AscensionStar/index.css.ts
+++ src/lib/components/AscensionStar/index.css.ts
@@ -1,5 +1,8 @@
+import { theme } from '$lib/styles/themes/index.css';
 import { style } from '@vanilla-extract/css';
 
 export const shadow = style({
 	filter: `drop-shadow(0px 0px 2px rgb(0 0 0 / 0.6))`,
+	width: theme.sizes[8],
+	height: theme.sizes[8],
 });
