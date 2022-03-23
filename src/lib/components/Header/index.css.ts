390675889ea1894e668d2b3c568a2fd8c5583271
Allan Galera <allangalera@gmail.com>
2022-03-23 14:04:30 -0300

    feat: Make header sticky

--- src/lib/components/Header/index.css.ts
+++ src/lib/components/Header/index.css.ts
@@ -1,7 +1,11 @@
 import { style } from '@vanilla-extract/css';
 import { theme } from '$lib/styles/themes/index.css';
 
-export const header = style({});
+export const header = style({
+	position: 'sticky',
+	top: 0,
+	zIndex: 1000,
+});
 
 export const container = style({
 	display: 'flex',
