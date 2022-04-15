00396674746ebd2dbb293813d3f530b6de074c67
Allan Galera <allangalera@gmail.com>
2022-04-15 10:32:40 -0300

    fix: wrong import

--- src/lib/components/Card/index.svelte
+++ src/lib/components/Card/index.svelte
@@ -3,7 +3,7 @@
   import type { Sprinkles } from '$lib/styles/sprinkles.css';
   import CardBorder from '$lib/components/CardBorder/index.svelte';
   import { getIdleKingdomNumberFormat } from '$lib/utils';
-  import { match } from 'oxide.ts/dist';
+  import { match } from 'oxide.ts';
   export let width: Sprinkles['width'] = 24;
   export let value: number = 0;
   export let cardType: CardType;
