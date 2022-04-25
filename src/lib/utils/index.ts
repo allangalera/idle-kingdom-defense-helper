c4eedc8690b869d8cde1e0ff4f4cc00563f14ebf
Allan Galera <allangalera@gmail.com>
2022-04-24 23:57:52 -0300

    feat: Use same function to round numbers in game

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -4,9 +4,174 @@ import type { Grades, UpgradeLevel } from '$lib/types';
 import { match } from 'oxide.ts';
 import { z } from 'zod';
 
-export const genericRounding = (value: number, n: number): number => {
-  const d = Math.pow(10, n);
-  return Math.round((value + Number.EPSILON) * d) / d;
+const DIGITS_STR = [
+  'K',
+  'M',
+  'B',
+  'T',
+  'aa',
+  'ab',
+  'ac',
+  'ad',
+  'ae',
+  'af',
+  'ag',
+  'ah',
+  'ai',
+  'aj',
+  'ak',
+  'al',
+  'am',
+  'an',
+  'ao',
+  'ap',
+  'aq',
+  'ar',
+  'as',
+  'at',
+  'au',
+  'av',
+  'aw',
+  'ax',
+  'ay',
+  'az',
+  'ba',
+  'bb',
+  'bc',
+  'bd',
+  'be',
+  'bf',
+  'bg',
+  'bh',
+  'bi',
+  'bj',
+  'bk',
+  'bl',
+  'bm',
+  'bn',
+  'bo',
+  'bp',
+  'bq',
+  'br',
+  'bs',
+  'bt',
+  'bu',
+  'bv',
+  'bw',
+  'bx',
+  'by',
+  'bz',
+  'ca',
+  'cb',
+  'cc',
+  'cd',
+  'ce',
+  'cf',
+  'cg',
+  'ch',
+  'ci',
+  'cj',
+  'ck',
+  'cl',
+  'cm',
+  'cn',
+  'co',
+  'cp',
+  'cq',
+  'cr',
+  'cs',
+  'ct',
+  'cu',
+  'cv',
+  'cw',
+  'cx',
+  'cy',
+  'cz',
+  'da',
+  'db',
+  'dc',
+  'dd',
+  'de',
+  'df',
+  'dg',
+  'dh',
+  'di',
+  'dj',
+  'dk',
+  'dl',
+  'dm',
+  'dn',
+  'do',
+  'dp',
+  'dq',
+  'dr',
+  'ds',
+  'dt',
+  'du',
+  'dv',
+  'dw',
+  'dx',
+  'dy',
+  'dz',
+  'ea',
+  'eb',
+  'ec',
+  'ed',
+  'ee',
+  'ef',
+  'eg',
+  'eh',
+  'ei',
+  'ej',
+  'ek',
+  'el',
+  'em',
+  'en',
+  'eo',
+  'ep',
+  'eq',
+  'er',
+  'es',
+  'et',
+  'eu',
+  'ev',
+  'ew',
+  'ex',
+  'ey',
+  'ez',
+  'fa',
+  'fb',
+  'fc',
+  'fd',
+  'fe',
+  'ff',
+  'fg',
+  'fh',
+  'fi',
+  'fj',
+  'fk',
+  'fl',
+  'fm',
+  'fn',
+  'fo',
+  'fp',
+  'fq',
+  'fr',
+  'fs',
+  'ft',
+  'fu',
+  'fv',
+  'fw',
+  'fx',
+  'fy',
+  'fz',
+];
+const DIGITS_NUMBER = 1000;
+const DIGITS_COUNT = Math.floor(Math.log10(DIGITS_NUMBER));
+
+export const genericRounding = (value: number, fixed: number): string => {
+  const re = new RegExp('^-?\\d+(?:.\\d{0,' + (fixed || -1) + '})?');
+  return value.toString().match(re)[0];
 };
 
 type GetIdleKingdomNumberFormat = (value: number, decimalNumbers?: number) => string;
@@ -15,21 +180,21 @@ export const getIdleKingdomNumberFormat: GetIdleKingdomNumberFormat = (
   value,
   decimalNumbers = 0
 ): string => {
-  let formattedString = genericRounding(value, decimalNumbers).toString();
-  if (value >= 10000000000) {
-    formattedString = genericRounding(value / 1000000000, decimalNumbers).toString() + 'B';
-  } else if (value >= 10000000) {
-    formattedString = genericRounding(value / 1000000, decimalNumbers).toString() + 'M';
-  } else if (value >= 100000) {
-    formattedString = genericRounding(value / 1000, decimalNumbers).toString() + 'K';
+  const formattedString = genericRounding(value, decimalNumbers);
+
+  if (value < 100000) {
+    return formattedString;
   }
 
-  return formattedString;
+  const unit = Math.floor((Math.log10(value) - 4) / DIGITS_COUNT) + 1;
+  const newValue = genericRounding(value / Math.pow(1000, unit), decimalNumbers).toString();
+
+  return `${newValue}${DIGITS_STR[unit - 1]}`;
 };
 
 export const heroUpgradeCostCalculatorParameterSchema = z
   .object({
-    currentLevel: z.number().gte(0),
+    currentLevel: z.number().gte(1),
   })
   .extend({
     targetLevel: z.number().lte(MAX_HERO_LEVEL),
