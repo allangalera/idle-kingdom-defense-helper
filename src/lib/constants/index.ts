b5079f16de0d27496c97a008b1783e37dc5e5c0f
Allan Galera <allangalera@gmail.com>
2022-05-16 13:33:07 -0300

    chore: Change max stage to 50k

--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,2 +1,2 @@
 export const CASTLE_MAX_LEVEL = 50000;
-export const MAX_STAGE_LEVEL = 15000;
+export const MAX_STAGE_LEVEL = 50000;
--- src/lib/db/runes.ts
+++ src/lib/db/runes.ts
@@ -1,10 +1,11 @@
+import { MAX_STAGE_LEVEL } from '$lib/constants';
 import runeJSON from '$lib/gameInfo/rune.json';
 import runeSummonJSON from '$lib/gameInfo/runeSummon.json';
 
 const getRunesSummonStageUnlock = () => {
   const rewards = {};
   for (const summon of runeSummonJSON) {
-    if (summon.stage > 50000) continue;
+    if (summon.stage > MAX_STAGE_LEVEL) continue;
     const currentMaxGrade = Math.max(...summon.result.grades);
     rewards[summon.id] = {
       grade: currentMaxGrade,
