2b8a21f0a24f6cde42229e95f246b51af8725c53
Allan Galera <allangalera@gmail.com>
2022-06-09 17:21:35 -0300

    fix: atkSpeed and movSpeed values on hero detail page

--- src/lib/utils/hero.ts
+++ src/lib/utils/hero.ts
@@ -1,5 +1,7 @@
-import { MAX_HERO_LEVEL, heroGradeInfo, heroLvCost, gears, type HeroType } from '$lib/db/heroes';
-import { Attributes, HeroGearEquipOptions, type HeroGearEquipTypes } from '$lib/enums';
+import { MAX_HERO_LEVEL, gears, heroGradeInfo, heroLvCost } from '$lib/db/heroes';
+import type { HeroType } from '$lib/db/heroes';
+import { Attributes, HeroGearEquipOptions } from '$lib/enums';
+import type { HeroGearEquipTypes } from '$lib/enums';
 import { getRuneById } from '$lib/utils/runes';
 import { match } from 'oxide.ts';
 import * as R from 'remeda';
@@ -78,41 +80,43 @@ const returnGearTypeAttributeKeys = (gearType: HeroGearEquipTypes) => {
     [HeroGearEquipOptions.helmet, [Attributes.hp, Attributes.criResist, Attributes.defPierce]],
     [HeroGearEquipOptions.chest, [Attributes.cri, Attributes.criDamage, Attributes.def]],
     [HeroGearEquipOptions.boots, [Attributes.dodge, Attributes.moveSpeed, Attributes.atkSpeed]],
-  ])
-}
+  ]);
+};
 
 const returnGearAttributesByGrade = (grade) => {
   return gears.find((gear) => gear.grade === grade);
-}
+};
 
 type HeroGears = Record<HeroGearEquipTypes, number | null>;
 
-const returnGearAttributes = (heroGears: HeroGears): Partial<Record<keyof typeof Attributes, number>> => {
-  let stats = {}
+const returnGearAttributes = (
+  heroGears: HeroGears
+): Partial<Record<keyof typeof Attributes, number>> => {
+  let stats = {};
   if (!heroGears) return stats;
 
   R.mapKeys(heroGears, (key, value) => {
     if (value) {
-      const gearAttr = returnGearAttributesByGrade(value)
+      const gearAttr = returnGearAttributesByGrade(value);
       const gearAttrKeys = returnGearTypeAttributeKeys(key);
-      stats = R.merge(stats, R.pick(gearAttr, gearAttrKeys))
+      stats = R.merge(stats, R.pick(gearAttr, gearAttrKeys));
     }
-    return 0
-  })
+    return 0;
+  });
 
-  return stats
-}
+  return stats;
+};
 
 const returnSetStats = (heroGears: HeroGears) => {
   if (!heroGears) return false;
 
   const isSet = new Set(Object.values(heroGears)).size === 1;
 
-  if (!isSet) return false
+  if (!isSet) return false;
 
-  const gearAttr = returnGearAttributesByGrade(heroGears.weapon)
-  return R.pick(gearAttr, ['setEffectValue1', 'setEffectValue2', 'setEffectValue3'])
-}
+  const gearAttr = returnGearAttributesByGrade(heroGears.weapon);
+  return R.pick(gearAttr, ['setEffectValue1', 'setEffectValue2', 'setEffectValue3']);
+};
 
 export const calculateHeroStats = (hero: HeroType, heroUserData) => {
   const heroStats = R.pick(hero, [
@@ -134,55 +138,56 @@ export const calculateHeroStats = (hero: HeroType, heroUserData) => {
     'moveSpeed',
   ]);
 
-  const firstCritDmgRune = getRuneById(1);
-  const firstAtkRune = getRuneById(2);
-
   const currentLevel = heroUserData?.level ?? 1;
 
   const currentGrade = heroUserData?.grade ?? hero.baseGrade;
 
   const currentAscension = hero.ascension.find((asc) => asc.grade === currentGrade);
 
-  heroStats.criDamage += firstCritDmgRune.abilityInitMin;
-
   heroStats.hp = (heroStats.hp + heroStats.incHp * (currentLevel - 1)) * currentAscension.incHp;
   heroStats.def = (heroStats.def + heroStats.incDef * (currentLevel - 1)) * currentAscension.incDef;
-  heroStats.atk =
-    (heroStats.atk + heroStats.incAtk * (currentLevel - 1)) * currentAscension.incAtk +
-    firstAtkRune.abilityInitMin;
+  heroStats.atk = (heroStats.atk + heroStats.incAtk * (currentLevel - 1)) * currentAscension.incAtk;
 
-    const equipStats = returnGearAttributes(heroUserData?.equip);
+  const equipStats = returnGearAttributes(heroUserData?.equip);
 
-    R.mapKeys(equipStats, (key, value) => {
-      heroStats[key] += value;
-      return 0
-    })
-
-    // apply set stats if available
-    const equipSetStats = returnSetStats(heroUserData?.equip);
-    if (equipSetStats) {
-      heroStats.criDamage *= (1 + equipSetStats.setEffectValue1)
-      heroStats.defPierce *= (1 + equipSetStats.setEffectValue2)
-      heroStats.atk *= (1 + equipSetStats.setEffectValue3)
+  R.mapKeys(equipStats, (key, value) => {
+    if ([Attributes.atkSpeed, Attributes.moveSpeed].some((attr) => attr === key)) {
+      return (heroStats[key] *= 1 + value);
     }
+    return (heroStats[key] += value);
+  });
+
+  // apply runes if available
+  const firstCritDmgRune = getRuneById(1);
+  const firstAtkRune = getRuneById(2);
+  heroStats.criDamage += firstCritDmgRune.abilityInitMin;
+  heroStats.atk += firstAtkRune.abilityInitMin;
+
+  // apply set stats if available
+  const equipSetStats = returnSetStats(heroUserData?.equip);
+  if (equipSetStats) {
+    heroStats.criDamage *= 1 + equipSetStats.setEffectValue1;
+    heroStats.defPierce *= 1 + equipSetStats.setEffectValue2;
+    heroStats.atk *= 1 + equipSetStats.setEffectValue3;
+  }
   return heroStats;
 };
 
 export const returnAttributeName = (attr: keyof typeof Attributes) => {
   return match(attr, [
-    [Attributes.hp, "HP"],
-    [Attributes.atk, "ATK"],
-    [Attributes.def, "DEF"],
-    [Attributes.cri, "CRIT Chance"],
-    [Attributes.criDamage, "CRIT DMG"],
-    [Attributes.criResist, "CRIT Resist"],
-    [Attributes.criDamageResist, "CRIT DMG Reduce"],
-    [Attributes.dodge, "DODGE"],
-    [Attributes.hit, "ACCURACY"],
-    [Attributes.defPierce, "DEF Penetration"],
-    [Attributes.defPierceResist, "Ignore DEF Penetration"],
-    [Attributes.atkSpeed, "Attack Speed"],
-    [Attributes.moveSpeed, "Movement Speed"],
+    [Attributes.hp, 'HP'],
+    [Attributes.atk, 'ATK'],
+    [Attributes.def, 'DEF'],
+    [Attributes.cri, 'CRIT Chance'],
+    [Attributes.criDamage, 'CRIT DMG'],
+    [Attributes.criResist, 'CRIT Resist'],
+    [Attributes.criDamageResist, 'CRIT DMG Reduce'],
+    [Attributes.dodge, 'DODGE'],
+    [Attributes.hit, 'ACCURACY'],
+    [Attributes.defPierce, 'DEF Penetration'],
+    [Attributes.defPierceResist, 'Ignore DEF Penetration'],
+    [Attributes.atkSpeed, 'Attack Speed'],
+    [Attributes.moveSpeed, 'Movement Speed'],
     () => attr,
-  ])
-}
+  ]);
+};
