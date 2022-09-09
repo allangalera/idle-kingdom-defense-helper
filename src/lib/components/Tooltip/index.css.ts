46ce27dc08fdb2e89f2bf006ba6eeced2bca9bf8
Allan Galera <allangalera@gmail.com>
2022-09-09 13:55:42 -0300

    feat: Improve tooltip behaviour

--- src/lib/components/Tooltip/index.css.ts
+++ src/lib/components/Tooltip/index.css.ts
@@ -3,27 +3,29 @@ import { style } from '@vanilla-extract/css';
 
 export const container = style({
   position: 'fixed',
-  zIndex: 1200,
-  transform: 'translate(-50%, -100%)',
+  zIndex: 9900,
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
-  width: theme.sizes.full,
 });
 
 export const tooltip = style({
-  background: theme.themeColors.baseColors.gray7,
+  background: theme.themeColors.baseColors.gray3,
   padding: theme.space[2],
   borderRadius: theme.border.radii.lg,
+  borderColor: theme.themeColors.baseColors.gray10,
+  borderWidth: theme.border.width.base,
+  borderStyle: 'solid',
 });
 
 export const triangle = style({
+  position: 'absolute',
   width: 0,
   height: 0,
   borderLeftColor: theme.colors.transparent,
   borderRightColor: theme.colors.transparent,
-  borderTopColor: theme.themeColors.baseColors.gray7,
+  borderTopColor: theme.themeColors.baseColors.gray10,
   borderLeftStyle: 'solid',
   borderRightStyle: 'solid',
   borderTopStyle: 'solid',
--- src/lib/components/Tooltip/index.svelte
+++ src/lib/components/Tooltip/index.svelte
@@ -1,34 +1,56 @@
 <script lang="ts">
   import Portal from '$lib/components/Portal/index.svelte';
   import Text from '$lib/components/Text/index.svelte';
+  import { autoPlacement, flip, offset, shift } from '@floating-ui/dom';
+  import { arrow, createFloatingActions } from 'svelte-floating-ui';
+  import { writable } from 'svelte/store';
 
   import * as styles from './index.css';
 
-  export let text = null;
+  export let text: string = null;
 
-  let open = false;
-  let el;
-  let x;
-  let y;
-  let maxWidth;
+  const arrowRef = writable(null);
+  const [floatingRef, floatingContent] = createFloatingActions({
+    strategy: 'fixed',
+    placement: 'top',
+    middleware: [shift({ padding: 8 }), flip(), offset(12), arrow({ element: arrowRef })],
+    autoUpdate: true,
+    onComputed(computed) {
+      if (!$arrowRef) return;
+      const { placement, middlewareData } = computed;
+      const { x, y } = middlewareData.arrow;
+      const currentPlacement = placement.split('-')[0];
+      const staticSide = {
+        top: 'bottom',
+        right: 'left',
+        bottom: 'top',
+        left: 'right',
+      }[currentPlacement];
+
+      const rotation = {
+        top: 'rotate(0deg)',
+        right: 'rotate(90deg)',
+        bottom: 'rotate(180deg)',
+        left: 'rotate(-90deg)',
+      }[currentPlacement];
+
+      Object.assign($arrowRef.style, {
+        left: x ? `${x}px` : 'unset',
+        top: y ? `${y}px` : 'unset',
+        transform: rotation,
+        [staticSide]: '-12px',
+      });
+    },
+  });
+
+  let showTooltip: boolean = false;
 
   const onShow = () => {
-    const boundingClientRect = el.getBoundingClientRect();
-    x = boundingClientRect.left + boundingClientRect.width / 2;
-    y = boundingClientRect.top;
-    const viewportWidth = Math.max(
-      document.documentElement.clientWidth || 0,
-      window.innerWidth || 0
-    );
-    const spaceOnTheLeft = (viewportWidth - x) * 2 - 16;
-    const spaceOnTheRight = (viewportWidth - (viewportWidth - x)) * 2 - 16;
-    maxWidth = Math.min(spaceOnTheLeft, spaceOnTheRight);
-
-    if (!open) open = true;
+    showTooltip = true;
   };
 
   const onHide = () => {
-    if (open) open = false;
+    showTooltip = false;
   };
 
   const onMouseOver = () => {
@@ -53,20 +75,22 @@
   on:mouseleave={onMouseLeave}
   on:focus={onFocus}
   on:blur={onBlur}
-  bind:this={el}
+  use:floatingRef
 >
   <slot />
 </span>
-{#if open}
+
+{#if showTooltip}
   <Portal>
-    <div class={styles.container} style="top: {y}px; left: {x}px; max-width: {maxWidth}px">
+    <div class={styles.container} use:floatingContent>
       <div class={styles.tooltip}>
-        <slot name="tooltip-content" />
         {#if text}
-          <Text textAlign="center">{text}</Text>
+          <Text>{text}</Text>
+        {:else}
+          <slot name="tooltip-content" />
         {/if}
       </div>
-      <div class={styles.triangle} />
+      <div class={styles.triangle} bind:this={$arrowRef} />
     </div>
   </Portal>
 {/if}
