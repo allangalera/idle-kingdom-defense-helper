512bec706c296eee218c79255759d9673ad5f1e8
Allan Galera <allangalera@gmail.com>
2022-11-02 21:06:41 -0300

    chore: Use old method to get env variable

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,6 @@
-import { env } from '$env/dynamic/public';
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 
-export const supabaseClient = createClient(
-	env.PUBLIC_SUPABASE_URL ?? '',
-	env.PUBLIC_SUPABASE_ANON_KEY ?? ''
-);
+const url = import.meta.env.VITE_SUPABASE_URL as string;
+const key = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+export const supabaseClient = createClient(url, key);
--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
 	plugins: [sveltekit(), vanillaExtractPlugin()],
-	define: {
-		'process.env': process.env
-	},
 	ssr: {
 		noExternal: ['@vanilla-extract/css', '@vanilla-extract/css/fileScope']
 	}
