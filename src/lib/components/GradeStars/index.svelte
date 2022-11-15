fe2d6858e237359534426a9f2139057b59ad93e8
Allan Galera <allangalera@gmail.com>
2022-11-15 19:39:42 -0300

    fix: Match change after update

--- src/lib/components/GradeStars/index.svelte
+++ src/lib/components/GradeStars/index.svelte
@@ -1,22 +1,23 @@
 <script lang="ts">
-  import { sprinkles } from '$lib/styles/sprinkles.css';
-  import type { Sprinkles } from '$lib/styles/sprinkles.css';
-  import { convertGradeToStarLevel } from '$lib/utils';
+	import { sprinkles } from '$lib/styles/sprinkles.css';
+	import type { Sprinkles } from '$lib/styles/sprinkles.css';
+	import { convertGradeToStarLevel } from '$lib/utils';
 
-  export let grade: number;
-  export let size: Sprinkles['width'] = 8;
+	export let grade: number;
+	export let size: Sprinkles['width'] = 8;
 
-  const data = convertGradeToStarLevel(grade);
+	const data = convertGradeToStarLevel(grade);
+	console.log(data);
 </script>
 
 {#each { length: data.level } as level}
-  <img
-    class={sprinkles({
-      width: size,
-      height: size,
-    })}
-    loading="lazy"
-    src={`images/icons/iconStarBig${Math.ceil(grade / 5)}.png`}
-    alt="icon"
-  />
+	<img
+		class={sprinkles({
+			width: size,
+			height: size
+		})}
+		loading="lazy"
+		src={`/images/icons/iconStarBig${Math.ceil(grade / 5)}.png`}
+		alt="icon"
+	/>
 {/each}
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -5,259 +5,261 @@ import { match } from 'oxide.ts';
 import { z } from 'zod';
 
 const DIGITS_STR = [
-  'K',
-  'M',
-  'B',
-  'T',
-  'aa',
-  'ab',
-  'ac',
-  'ad',
-  'ae',
-  'af',
-  'ag',
-  'ah',
-  'ai',
-  'aj',
-  'ak',
-  'al',
-  'am',
-  'an',
-  'ao',
-  'ap',
-  'aq',
-  'ar',
-  'as',
-  'at',
-  'au',
-  'av',
-  'aw',
-  'ax',
-  'ay',
-  'az',
-  'ba',
-  'bb',
-  'bc',
-  'bd',
-  'be',
-  'bf',
-  'bg',
-  'bh',
-  'bi',
-  'bj',
-  'bk',
-  'bl',
-  'bm',
-  'bn',
-  'bo',
-  'bp',
-  'bq',
-  'br',
-  'bs',
-  'bt',
-  'bu',
-  'bv',
-  'bw',
-  'bx',
-  'by',
-  'bz',
-  'ca',
-  'cb',
-  'cc',
-  'cd',
-  'ce',
-  'cf',
-  'cg',
-  'ch',
-  'ci',
-  'cj',
-  'ck',
-  'cl',
-  'cm',
-  'cn',
-  'co',
-  'cp',
-  'cq',
-  'cr',
-  'cs',
-  'ct',
-  'cu',
-  'cv',
-  'cw',
-  'cx',
-  'cy',
-  'cz',
-  'da',
-  'db',
-  'dc',
-  'dd',
-  'de',
-  'df',
-  'dg',
-  'dh',
-  'di',
-  'dj',
-  'dk',
-  'dl',
-  'dm',
-  'dn',
-  'do',
-  'dp',
-  'dq',
-  'dr',
-  'ds',
-  'dt',
-  'du',
-  'dv',
-  'dw',
-  'dx',
-  'dy',
-  'dz',
-  'ea',
-  'eb',
-  'ec',
-  'ed',
-  'ee',
-  'ef',
-  'eg',
-  'eh',
-  'ei',
-  'ej',
-  'ek',
-  'el',
-  'em',
-  'en',
-  'eo',
-  'ep',
-  'eq',
-  'er',
-  'es',
-  'et',
-  'eu',
-  'ev',
-  'ew',
-  'ex',
-  'ey',
-  'ez',
-  'fa',
-  'fb',
-  'fc',
-  'fd',
-  'fe',
-  'ff',
-  'fg',
-  'fh',
-  'fi',
-  'fj',
-  'fk',
-  'fl',
-  'fm',
-  'fn',
-  'fo',
-  'fp',
-  'fq',
-  'fr',
-  'fs',
-  'ft',
-  'fu',
-  'fv',
-  'fw',
-  'fx',
-  'fy',
-  'fz',
+	'K',
+	'M',
+	'B',
+	'T',
+	'aa',
+	'ab',
+	'ac',
+	'ad',
+	'ae',
+	'af',
+	'ag',
+	'ah',
+	'ai',
+	'aj',
+	'ak',
+	'al',
+	'am',
+	'an',
+	'ao',
+	'ap',
+	'aq',
+	'ar',
+	'as',
+	'at',
+	'au',
+	'av',
+	'aw',
+	'ax',
+	'ay',
+	'az',
+	'ba',
+	'bb',
+	'bc',
+	'bd',
+	'be',
+	'bf',
+	'bg',
+	'bh',
+	'bi',
+	'bj',
+	'bk',
+	'bl',
+	'bm',
+	'bn',
+	'bo',
+	'bp',
+	'bq',
+	'br',
+	'bs',
+	'bt',
+	'bu',
+	'bv',
+	'bw',
+	'bx',
+	'by',
+	'bz',
+	'ca',
+	'cb',
+	'cc',
+	'cd',
+	'ce',
+	'cf',
+	'cg',
+	'ch',
+	'ci',
+	'cj',
+	'ck',
+	'cl',
+	'cm',
+	'cn',
+	'co',
+	'cp',
+	'cq',
+	'cr',
+	'cs',
+	'ct',
+	'cu',
+	'cv',
+	'cw',
+	'cx',
+	'cy',
+	'cz',
+	'da',
+	'db',
+	'dc',
+	'dd',
+	'de',
+	'df',
+	'dg',
+	'dh',
+	'di',
+	'dj',
+	'dk',
+	'dl',
+	'dm',
+	'dn',
+	'do',
+	'dp',
+	'dq',
+	'dr',
+	'ds',
+	'dt',
+	'du',
+	'dv',
+	'dw',
+	'dx',
+	'dy',
+	'dz',
+	'ea',
+	'eb',
+	'ec',
+	'ed',
+	'ee',
+	'ef',
+	'eg',
+	'eh',
+	'ei',
+	'ej',
+	'ek',
+	'el',
+	'em',
+	'en',
+	'eo',
+	'ep',
+	'eq',
+	'er',
+	'es',
+	'et',
+	'eu',
+	'ev',
+	'ew',
+	'ex',
+	'ey',
+	'ez',
+	'fa',
+	'fb',
+	'fc',
+	'fd',
+	'fe',
+	'ff',
+	'fg',
+	'fh',
+	'fi',
+	'fj',
+	'fk',
+	'fl',
+	'fm',
+	'fn',
+	'fo',
+	'fp',
+	'fq',
+	'fr',
+	'fs',
+	'ft',
+	'fu',
+	'fv',
+	'fw',
+	'fx',
+	'fy',
+	'fz'
 ];
 const DIGITS_NUMBER = 1000;
 const DIGITS_COUNT = Math.floor(Math.log10(DIGITS_NUMBER));
 
 export const genericRounding: (value: number, fixed?: number) => string = (value, fixed = 0) => {
-  if (!value) return '0';
-  const re = new RegExp('^-?\\d+(?:.\\d{0,' + (fixed || -1) + '})?');
-  return value.toString().match(re)[0];
+	if (!value) return '0';
+	const re = new RegExp('^-?\\d+(?:.\\d{0,' + (fixed || -1) + '})?');
+	return value.toString().match(re)[0];
 };
 
 type GetIdleKingdomNumberFormat = (value: number, decimalNumbers?: number) => string;
 
 export const getIdleKingdomNumberFormat: GetIdleKingdomNumberFormat = (
-  value,
-  decimalNumbers = 0
+	value,
+	decimalNumbers = 0
 ): string => {
-  const formattedString = genericRounding(value, decimalNumbers);
+	const formattedString = genericRounding(value, decimalNumbers);
 
-  if (value < 100000) {
-    return formattedString;
-  }
+	if (value < 100000) {
+		return formattedString;
+	}
 
-  const unit = Math.floor((Math.log10(value) - 4) / DIGITS_COUNT) + 1;
-  const newValue = genericRounding(value / Math.pow(1000, unit), decimalNumbers).toString();
+	const unit = Math.floor((Math.log10(value) - 4) / DIGITS_COUNT) + 1;
+	const newValue = genericRounding(value / Math.pow(1000, unit), decimalNumbers).toString();
 
-  return `${newValue}${DIGITS_STR[unit - 1]}`;
+	return `${newValue}${DIGITS_STR[unit - 1]}`;
 };
 
 export const heroUpgradeCostCalculatorParameterSchema = z
-  .object({
-    currentLevel: z.number().gte(1),
-  })
-  .extend({
-    targetLevel: z.number().lte(MAX_HERO_LEVEL),
-  })
-  .refine((value) => value.currentLevel < value.targetLevel, {
-    message: 'Current level need to be higher than Target level',
-  });
+	.object({
+		currentLevel: z.number().gte(1)
+	})
+	.extend({
+		targetLevel: z.number().lte(MAX_HERO_LEVEL)
+	})
+	.refine((value) => value.currentLevel < value.targetLevel, {
+		message: 'Current level need to be higher than Target level'
+	});
 
 export const romanize = (num: number) => {
-  if (isNaN(num)) return NaN;
-  const digits = String(+num).split('');
-  const key = [
-    '',
-    'C',
-    'CC',
-    'CCC',
-    'CD',
-    'D',
-    'DC',
-    'DCC',
-    'DCCC',
-    'CM',
-    '',
-    'X',
-    'XX',
-    'XXX',
-    'XL',
-    'L',
-    'LX',
-    'LXX',
-    'LXXX',
-    'XC',
-    '',
-    'I',
-    'II',
-    'III',
-    'IV',
-    'V',
-    'VI',
-    'VII',
-    'VIII',
-    'IX',
-  ];
-  let roman = '';
-  let i = 3;
-  while (i--) roman = (key[+digits.pop() + i * 10] || '') + roman;
-  return Array(+digits.join('') + 1).join('M') + roman;
+	if (isNaN(num)) return NaN;
+	const digits = String(+num).split('');
+	const key = [
+		'',
+		'C',
+		'CC',
+		'CCC',
+		'CD',
+		'D',
+		'DC',
+		'DCC',
+		'DCCC',
+		'CM',
+		'',
+		'X',
+		'XX',
+		'XXX',
+		'XL',
+		'L',
+		'LX',
+		'LXX',
+		'LXXX',
+		'XC',
+		'',
+		'I',
+		'II',
+		'III',
+		'IV',
+		'V',
+		'VI',
+		'VII',
+		'VIII',
+		'IX'
+	];
+	let roman = '';
+	let i = 3;
+	while (i--) roman = (key[+digits.pop() + i * 10] || '') + roman;
+	return Array(+digits.join('') + 1).join('M') + roman;
 };
 
 export const convertGradeToStarLevel = (
-  grade: number
+	grade: number
 ): { rarity: RarityEnum; level: UpgradeLevel } => {
-  const level = match(grade % 5, [[0, 5], (n) => n]) as UpgradeLevel;
+	const level = match(grade % 5, [[0, 5], () => grade % 5]) as UpgradeLevel;
 
-  const rarity = match(grade / 5, [
-    [(n) => n <= 1, RarityEnum.common],
-    [(n) => n <= 2, RarityEnum.uncommon],
-    [(n) => n <= 3, RarityEnum.rare],
-    [(n) => n <= 4, RarityEnum.epic],
-    [(n) => n <= 5, RarityEnum.legendary],
-    () => RarityEnum.mythic,
-  ]);
+	// console.log({ grade, levelMatch: grade % 5, level });
 
-  return { rarity, level };
+	const rarity = match(grade / 5, [
+		[(n) => n <= 1, RarityEnum.common],
+		[(n) => n <= 2, RarityEnum.uncommon],
+		[(n) => n <= 3, RarityEnum.rare],
+		[(n) => n <= 4, RarityEnum.epic],
+		[(n) => n <= 5, RarityEnum.legendary],
+		() => RarityEnum.mythic
+	]);
+
+	return { rarity, level };
 };
