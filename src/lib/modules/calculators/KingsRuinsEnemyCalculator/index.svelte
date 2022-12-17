bc0188acedb7be614e4720ae27dce26e11c7078e
Allan Galera Teixeira <allangalera@gmail.com>
2022-12-17 18:14:58 -0300

    Sveltekit upgrade v1.0 (#23)
    * update dependencies
    
    * remove unecessary package | update vite & sveltekit
    
    * feat: Add checkbox to hide sweepable stages on KR calc
    
    * chore: Add  @sveltejs/adapter-vercel because it needs on vercel build process

--- src/lib/modules/calculators/KingsRuinsEnemyCalculator/index.svelte
+++ src/lib/modules/calculators/KingsRuinsEnemyCalculator/index.svelte
@@ -1,99 +1,115 @@
 <script lang="ts">
-  import Button from '$lib/components/Button/index.svelte';
-  import Input from '$lib/components/Input/index.svelte';
-  import Text from '$lib/components/Text/index.svelte';
-  import { MAX_STAGE_LEVEL } from '$lib/constants';
-  import { ruinConst } from '$lib/db/ruins';
-  import {
-    calculatorsInformationStore,
-    updateCalculatorInformation,
-  } from '$lib/shared/stores/user/calculatorsInformation';
-  import { theme } from '$lib/styles/themes/index.css';
-  import { getEnemyPattern } from '$lib/utils/ruins';
-  import Icon from 'svelte-icons-pack/Icon.svelte';
-  import RiSystemArrowLeftSLine from 'svelte-icons-pack/ri/RiSystemArrowLeftSLine';
-  import RiSystemArrowRightSLine from 'svelte-icons-pack/ri/RiSystemArrowRightSLine';
+	import Button from '$lib/components/Button/index.svelte';
+	import Input from '$lib/components/Input/index.svelte';
+	import Text from '$lib/components/Text/index.svelte';
+	import Toggle from '$lib/components/Toggle/index.svelte';
+	import { MAX_STAGE_LEVEL } from '$lib/constants';
+	import { ruinConst } from '$lib/db/ruins';
+	import {
+		calculatorsInformationStore,
+		updateCalculatorInformation
+	} from '$lib/shared/stores/user/calculatorsInformation';
+	import { sprinkles } from '$lib/styles/sprinkles.css';
+	import { theme } from '$lib/styles/themes/index.css';
+	import { getEnemyPattern } from '$lib/utils/ruins';
+	import * as styles from './index.css';
+	import Icon from 'svelte-icons-pack/Icon.svelte';
+	import RiSystemArrowLeftSLine from 'svelte-icons-pack/ri/RiSystemArrowLeftSLine';
+	import RiSystemArrowRightSLine from 'svelte-icons-pack/ri/RiSystemArrowRightSLine';
 
-  import * as styles from './index.css';
+	const CALCULATOR_STORE_KEY = 'KINGS_RUIN_ENEMY_CALCULATOR';
 
-  const CALCULATOR_STORE_KEY = 'KINGS_RUIN_ENEMY_CALCULATOR';
+	let stageLevel = $calculatorsInformationStore?.[CALCULATOR_STORE_KEY]?.stageLevel ?? '1';
+	let enemyPattern: ReturnType<typeof getEnemyPattern> = [];
+	let showOnlyWithCashReward: boolean = false;
 
-  let stageLevel = $calculatorsInformationStore?.[CALCULATOR_STORE_KEY]?.stageLevel ?? '1';
-  let enemyPattern = [];
+	const updateEnemyPattern = () => {
+		let stageLevelAsNumber = +stageLevel;
+		if (stageLevelAsNumber) enemyPattern = getEnemyPattern(stageLevelAsNumber);
+	};
 
-  const updateEnemyPattern = () => {
-    let stageLevelAsNumber = +stageLevel;
-    if (stageLevelAsNumber) enemyPattern = getEnemyPattern(stageLevelAsNumber);
-  };
+	const onAddStageLevel = () => {
+		stageLevel = Math.min(+stageLevel + 1, MAX_STAGE_LEVEL).toString();
+	};
 
-  const onAddStageLevel = () => {
-    stageLevel = Math.min(+stageLevel + 1, MAX_STAGE_LEVEL).toString();
-  };
+	const onRemoveStageLevel = () => {
+		stageLevel = Math.max(1, +stageLevel - 1).toString();
+	};
 
-  const onRemoveStageLevel = () => {
-    stageLevel = Math.max(1, +stageLevel - 1).toString();
-  };
+	const updateStore = () => {
+		updateCalculatorInformation(CALCULATOR_STORE_KEY, {
+			stageLevel
+		});
+	};
 
-  const updateStore = () => {
-    updateCalculatorInformation(CALCULATOR_STORE_KEY, {
-      stageLevel,
-    });
-  };
+	const filterEnemyPatters = () => {
+		if (showOnlyWithCashReward) {
+			enemyPattern = enemyPattern.filter((pattern) => !!ruinConst.REWARD.CASH[pattern.id - 1]);
+		}
+	};
 
-  const update = () => {
-    updateEnemyPattern();
-    updateStore();
-  };
+	const update = () => {
+		updateEnemyPattern();
+		updateStore();
+		filterEnemyPatters();
+	};
 
-  $: stageLevel && update();
+	$: (stageLevel || showOnlyWithCashReward) && update();
 </script>
 
 <div class={styles.container}>
-  <div class={styles.inputContainer}>
-    <Button variant="primary" on:click={onRemoveStageLevel} disabled={+stageLevel === 1}>
-      <Icon className={styles.menuIcon} src={RiSystemArrowLeftSLine} color={theme.colors.white} />
-    </Button>
-    <Input
-      label="Stage when the game daily reset"
-      textAlign="center"
-      bind:value={stageLevel}
-      maskOptions={{
-        mask: Number,
-        min: 0,
-        max: MAX_STAGE_LEVEL,
-      }}
-    />
-    <Button on:click={onAddStageLevel} disabled={+stageLevel === MAX_STAGE_LEVEL}>
-      <Icon className={styles.menuIcon} src={RiSystemArrowRightSLine} color={theme.colors.white} />
-    </Button>
-  </div>
-  <Text>Enemies:</Text>
-  <div class={styles.resultsContainer}>
-    {#each enemyPattern as enemies, idx}
-      <div class={styles.flex}>
-        <div>
-          <Text fontSize="lg" textAlign="center">{idx + 1}</Text>
-          {#if ruinConst.REWARD.CASH[idx]}
-            <Text
-              >{ruinConst.REWARD.CASH[idx]}<img
-                class={styles.icon}
-                loading="lazy"
-                src={`images/icons/iconRuby.png`}
-                alt="enemy"
-              /></Text
-            >
-          {/if}
-        </div>
-        {#each Object.keys(enemies) as enemy}
-          {#if enemies[enemy].unitType === 5}
-            <img
-              loading="lazy"
-              src={`images/enemies/enemy${enemies[enemy].unitId}.png`}
-              alt="enemy"
-            />
-          {/if}
-        {/each}
-      </div>
-    {/each}
-  </div>
+	<div class={styles.inputContainer}>
+		<Button variant="primary" on:click={onRemoveStageLevel} disabled={+stageLevel === 1}>
+			<Icon className={styles.menuIcon} src={RiSystemArrowLeftSLine} color={theme.colors.white} />
+		</Button>
+		<Input
+			label="Stage when the game daily reset"
+			textAlign="center"
+			bind:value={stageLevel}
+			maskOptions={{
+				mask: Number,
+				min: 0,
+				max: MAX_STAGE_LEVEL
+			}}
+		/>
+		<Button on:click={onAddStageLevel} disabled={+stageLevel === MAX_STAGE_LEVEL}>
+			<Icon className={styles.menuIcon} src={RiSystemArrowRightSLine} color={theme.colors.white} />
+		</Button>
+		<div
+			class={sprinkles({
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				justifyContent: 'center'
+			})}
+		>
+			<Text>Hide sweepable</Text>
+			<Toggle bind:checked={showOnlyWithCashReward} />
+		</div>
+	</div>
+	<Text>Enemies:</Text>
+	<div class={styles.resultsContainer}>
+		{#each enemyPattern as pattern, idx}
+			<div class={styles.flex}>
+				<div>
+					<Text fontSize="lg" textAlign="center">{pattern.id}</Text>
+					{#if ruinConst.REWARD.CASH[pattern.id - 1]}
+						<Text
+							>{ruinConst.REWARD.CASH[pattern.id - 1]}<img
+								class={styles.icon}
+								loading="lazy"
+								src={`images/icons/iconRuby.png`}
+								alt="enemy"
+							/></Text
+						>
+					{/if}
+				</div>
+				{#each pattern.enemies as enemy}
+					{#if enemy.unitType === 5}
+						<img loading="lazy" src={`images/enemies/enemy${enemy.unitId}.png`} alt="enemy" />
+					{/if}
+				{/each}
+			</div>
+		{/each}
+	</div>
 </div>
--- src/lib/pages/index.svelte
+++ src/lib/pages/index.svelte
@@ -1,47 +1,47 @@
 <script>
-  import * as styles from './index.css';
-  import GridItem from '$lib/components/GridItem/index.svelte';
-  import CastleGoldUpgradeCostCalculator from '$lib/modules/calculators/CastleGoldUpgradeCostCalculator/index.svelte';
-  import HeroUpgradeCostCalculator from '$lib/modules/calculators/HeroUpgradeCostCalculator/index.svelte';
-  import HeroAscensionCalculator from '$lib/modules/calculators/HeroAscensionCalculator/index.svelte';
-  import FarmStageCalculator from '$lib/modules/calculators/FarmStageCalculator/index.svelte';
-  import ArcherEquipUpgradeCalculator from '$lib/modules/calculators/ArcherEquipUpgradeCalculator/index.svelte';
-  import ArcherEquipLevelCompareCalculator from '$lib/modules/calculators/ArcherEquipLevelCompareCalculator/index.svelte';
-  import StageFarmCompareCalculator from '$lib/modules/calculators/StageFarmCompareCalculator/index.svelte';
-  import KingsRuinsEnemyCalculator from '$lib/modules/calculators/KingsRuinsEnemyCalculator/index.svelte';
-  import CastleEngraveCalculator from '$lib/modules/calculators/CastleEngraveCalculator/index.svelte';
-  import ArtifactStatusCalculator from '$lib/modules/calculators/ArtifactStatusCalculator/index.svelte';
+	import * as styles from './index.css';
+	import GridItem from '$lib/components/GridItem/index.svelte';
+	import CastleGoldUpgradeCostCalculator from '$lib/modules/calculators/CastleGoldUpgradeCostCalculator/index.svelte';
+	import HeroUpgradeCostCalculator from '$lib/modules/calculators/HeroUpgradeCostCalculator/index.svelte';
+	import HeroAscensionCalculator from '$lib/modules/calculators/HeroAscensionCalculator/index.svelte';
+	import FarmStageCalculator from '$lib/modules/calculators/FarmStageCalculator/index.svelte';
+	import ArcherEquipUpgradeCalculator from '$lib/modules/calculators/ArcherEquipUpgradeCalculator/index.svelte';
+	import ArcherEquipLevelCompareCalculator from '$lib/modules/calculators/ArcherEquipLevelCompareCalculator/index.svelte';
+	import StageFarmCompareCalculator from '$lib/modules/calculators/StageFarmCompareCalculator/index.svelte';
+	import KingsRuinsEnemyCalculator from '$lib/modules/calculators/KingsRuinsEnemyCalculator/index.svelte';
+	import CastleEngraveCalculator from '$lib/modules/calculators/CastleEngraveCalculator/index.svelte';
+	import ArtifactStatusCalculator from '$lib/modules/calculators/ArtifactStatusCalculator/index.svelte';
 </script>
 
 <div class={styles.grid}>
-  <GridItem title="Castle Upgrade Cost Calculator">
-    <CastleGoldUpgradeCostCalculator />
-  </GridItem>
-  <GridItem title="Hero Upgrade Cost Calculator">
-    <HeroUpgradeCostCalculator />
-  </GridItem>
-  <GridItem title="Hero Ascension Calculator">
-    <HeroAscensionCalculator />
-  </GridItem>
-  <GridItem title="Archer Promote Cost Calculator">
-    <ArcherEquipUpgradeCalculator />
-  </GridItem>
-  <GridItem title="Artifact Status Calculator" full>
-    <ArtifactStatusCalculator />
-  </GridItem>
-  <GridItem title="Castle Engraving Calculator" full>
-    <CastleEngraveCalculator />
-  </GridItem>
-  <GridItem title="Archer Equip Compare Calculator" full>
-    <ArcherEquipLevelCompareCalculator />
-  </GridItem>
-  <GridItem title="Stage Farm Compare Calculator" full>
-    <StageFarmCompareCalculator />
-  </GridItem>
-  <GridItem title="Stage to Farm Calculator" full>
-    <FarmStageCalculator />
-  </GridItem>
-  <GridItem title="King's Ruins Enemy Calculator" full>
-    <KingsRuinsEnemyCalculator />
-  </GridItem>
+	<GridItem title="Castle Upgrade Cost Calculator">
+		<CastleGoldUpgradeCostCalculator />
+	</GridItem>
+	<GridItem title="Hero Upgrade Cost Calculator">
+		<HeroUpgradeCostCalculator />
+	</GridItem>
+	<GridItem title="Hero Ascension Calculator">
+		<HeroAscensionCalculator />
+	</GridItem>
+	<GridItem title="Archer Promote Cost Calculator">
+		<ArcherEquipUpgradeCalculator />
+	</GridItem>
+	<GridItem title="Artifact Status Calculator" full>
+		<ArtifactStatusCalculator />
+	</GridItem>
+	<GridItem title="Castle Engraving Calculator" full>
+		<CastleEngraveCalculator />
+	</GridItem>
+	<GridItem title="Archer Equip Compare Calculator" full>
+		<ArcherEquipLevelCompareCalculator />
+	</GridItem>
+	<GridItem title="Stage Farm Compare Calculator" full>
+		<StageFarmCompareCalculator />
+	</GridItem>
+	<GridItem title="Stage to Farm Calculator" full>
+		<FarmStageCalculator />
+	</GridItem>
+	<GridItem title="King's Ruins Enemy Calculator" full>
+		<KingsRuinsEnemyCalculator />
+	</GridItem>
 </div>
