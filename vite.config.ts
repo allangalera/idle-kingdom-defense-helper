c84ba860ed8663059b6f951bab5c3a043977251c
Allan Galera <allangalera@gmail.com>
2023-07-15 00:50:28 +0200

    feat: Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import type { UserConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 const config: UserConfig = {
 	plugins: [sveltekit(), vanillaExtractPlugin(), tsconfigPaths()],
@@ -9,8 +9,8 @@ const config: UserConfig = {
 	// 	include: ['src/**/*.{test,spec}.{js,ts}']
 	// },
 	ssr: {
-		noExternal: ['@vanilla-extract/css', '@vanilla-extract/css/fileScope']
-	}
+		noExternal: ['@vanilla-extract/css', '@vanilla-extract/css/fileScope'],
+	},
 };
 
 export default config;
